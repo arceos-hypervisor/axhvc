ac21e8db12eb6c00bde978ee91e1a51f55a18818
hky1999 <976929993@qq.com>
2025-07-11 17:03:43 +0800

    [feat] remove HLoadMMap

--- src/lib.rs
+++ src/lib.rs
@@ -23,12 +23,6 @@ numeric_enum! {
         HInitShim = HYPER_CALL_CODE_PRIVILEGED_MASK | 1,
         /// Create a new instance, pass the raw binary/executable file by shared pages.
         HCreateInstance = HYPER_CALL_CODE_PRIVILEGED_MASK |2,
-        /// Map instance memory to the host guest virtual address space.
-        /// This is called by the kernel driver when loading instance's ELF file.
-        /// called by host Linux, while the mapping will be synced to the instance.
-        /// This is used to map the host file system to the instance.
-        /// The file is mapped to the instance file memory region, and the instance can access it.
-        HLoadMMap = HYPER_CALL_CODE_PRIVILEGED_MASK | 3,
         /// Setup a instance, this is called by the instance when it is created and loaded.
         HSetupInstance = HYPER_CALL_CODE_PRIVILEGED_MASK | 4,
         /// Exit from a insance process.
@@ -59,7 +53,6 @@ impl core::fmt::Debug for HyperCallCode {
             HyperCallCode::HCreateInstance => write!(f, "HCreateInstance {:#x}", *self as u32),
             HyperCallCode::HExitProcess => write!(f, "HExitProcess {:#x}", *self as u32),
             HyperCallCode::HShutdownInstance => write!(f, "HShutdownInstance {:#x}", *self as u32),
-            HyperCallCode::HLoadMMap => write!(f, "HLoadMMap {:#x}", *self as u32),
             HyperCallCode::HClone => write!(f, "HClone {:#x}", *self as u32),
             HyperCallCode::HInitShim => write!(f, "HInitShim {:#x}", *self as u32),
             HyperCallCode::HSetupInstance => write!(f, "HSetupInstance {:#x}", *self as u32),
